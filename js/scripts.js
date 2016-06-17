f1d1bf545339e94faab37c2fec74e41e2156ac75
agustindavid <agustindavid@gmail.com>
2016-06-17 08:55:43 -0400

    desktot with instagram play

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,24 @@ $(document).ready(function(){
     });
     
     $('.close-content').click(function(){
-            $('.new-content').slideToggle();
+        $('.new-content').slideToggle();
+        $('.new-item').removeClass('active');
+    });
+    
+    $('.md-trigger').click(function(){
+        var modal_image=$(this).data('big');
+            $('.modal-img img').attr('src', modal_image);
+    });
+    
+    $('.tweet-media').click(function(){
+        var video_ig= $('video', this).get(0);
+        $('.play-btn', this).hide();
+        if(video_ig.paused){
+        video_ig.play();
+        } else {
+            video_ig.pause();
+            $('.play-btn', this).show();
+        }
     });
 });
 
