19f590d2ceda4aeb75d5bd39cd944de25037c6fb
agustindavid <agustindavid@gmail.com>
2016-12-07 17:58:03 -0400

    carrusel

--- especial_doblaje/js/scripts.js
+++ especial_doblaje/js/scripts.js
@@ -6,9 +6,26 @@ $(document).ready(function(){
       scale      : 0.8
     };
       window.sr = ScrollReveal();
-      sr.reveal('.main-posts-wrap', config);
       sr.reveal('.dark-gray-home', config); 
       sr.reveal('.huge-links-bg', config); 
       sr.reveal('#directorioCarousel', config);     
+       
+    $('.purple-share').click(function(){
+      $(this).closest('.home-post-wrap').find('.share-thumb').addClass('visible');
+    });
+    
+    $('.close-share').click(function(){
+      $(this).closest('.share-thumb').removeClass('visible');
+    });
+    
+    $('#directorioCarousel').bind('slid.bs.carousel', function (e) {
+        var distance_top=$('.posts-section').offset();
+                            $('html, body').delay(100).animate({
+                        scrollTop: ($('.posts-section').offset().top)-120
+                    }, 500);
+        console.log(distance_top.top);
+    });
+    
     
 });
+
