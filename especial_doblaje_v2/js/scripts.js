542181ba2193473c6e49fa32e8a48e9ac6f572a1
agustindavid <agustindavid@gmail.com>
2016-12-07 21:32:25 -0400

    no reveal

--- especial_doblaje_v2/js/scripts.js
+++ especial_doblaje_v2/js/scripts.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    var config = {
-      viewFactor : 0.15,
-      duration   : 1000,
-      distance   : "40px",
-      scale      : 0.8
-    };
-      window.sr = ScrollReveal();
-      sr.reveal('.dark-gray-home', config); 
-      sr.reveal('.huge-links-bg', config); 
-      sr.reveal('#directorioCarousel', config);     
-       
     $('.purple-share').click(function(){
       $(this).closest('.home-post-wrap').find('.share-thumb').addClass('visible');
     });
