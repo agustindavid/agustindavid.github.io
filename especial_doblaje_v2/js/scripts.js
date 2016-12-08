e0eecee95e11e7a9a2a817777386850bcc2cb32c
agustindavid <agustindavid@gmail.com>
2016-12-08 12:24:22 -0400

    dsfsdfsfd

--- especial_doblaje_v2/js/scripts.js
+++ especial_doblaje_v2/js/scripts.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
       $(this).closest('.share-thumb').removeClass('visible');
     });
     
-        $('#directoriohomeCarousel').bind('slid.bs.carousel', function (e) {
+        $('#directoriohomeCarousel').bind('slide.bs.carousel', function (e) {
         var distance_top=$('.posts-section').offset();
-                            $('html, body').delay(100).animate({
+                            $('html, body').delay(0).animate({
                         scrollTop: ($('.posts-section').offset().top)-120
                     }, 500);
         console.log(distance_top.top);
